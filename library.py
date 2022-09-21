1b5726973a70da821cb4dddccd0db87d8ebe1e21
maDiv9820 <themathur901@gmail.com>
2022-09-21 11:25:11 +0530

    Contains all the libraries required

---
+++ library.py
@@ -0,0 +1,19 @@
+import pandas as pd     # For opening and creating dataframes
+import numpy as np      # For creating mathematical and array operations
+import boto3            # For using AWS services in python
+import json             # Converting from Python to JSON format or vice-versa
+import sqlite3          # For creating a database and dumping data into the table
+import configparser     # To read config files for hard-coded values
+
+# Taking out all the values from config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+samsung_headers = eval(config.get('Script1', 'samsung_headers'))
+fitbit_headers = eval(config.get('Script1', 'fitbit_headers'))
+file_paths = eval(config.get('Script1', 'file_paths'))
+queue_url = config.get('AWS', 'queue_url')
+endpoint_url = config.get('AWS', 'endpoint_url')
+data_headers = eval(config.get('Script2', 'data_headers'))
+database_name = config.get('Script2', 'database_name')
+table_name = config.get('Script2', 'table_name')
